501ef25af97ea94f8187a38cd7f88a784a9090fc
Angie <guanshenbao@wbiao.cn>
2018-09-04 17:45:17 +0800

    添加代码

---
+++ CocoaPodsTest/CocoaPodsTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  CocoaPodsTest
+//
+//  Created by Angie on 2018/9/4.
+//  Copyright © 2018年 Angie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"什么都没有。。就是测试一下cocoaPods");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
